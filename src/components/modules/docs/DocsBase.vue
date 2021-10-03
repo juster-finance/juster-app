605492c9b98d059afa0178410d6d53830276be3c
Sunlight <celldwellerq@gmail.com>
2021-10-03 23:20:29 +0300

    article :slug

--- src/components/modules/docs/DocsBase.vue
+++ src/components/modules/docs/DocsBase.vue
@@ -1,22 +1,28 @@
 <script>
-import { defineComponent, onMounted, ref, computed } from "vue"
+import { defineComponent, onMounted, ref } from "vue"
+import { useRouter } from "vue-router"
 import Markdown from "markdown-it"
-
-import ArticleContent from "./ArticleContent"
 
 /**
  * API
  */
 import { fetchArticles, fetchSections } from "@/api/sanity"
 
+/**
+ * Store
+ */
+import { useDocsStore } from "@/store/docs"
+
 export default defineComponent({
     name: "DocsBase",
 
     setup() {
+        const router = useRouter()
+
+        const docsStore = useDocsStore()
+
         const expandMenu = ref(false)
 
-        const sections = ref({})
-        const selectedArticle = ref({})
         const anchors = ref([])
 
         onMounted(async () => {
@@ -27,8 +33,8 @@ export default defineComponent({
             allSections
                 .sort((a, b) => a.position - b.position)
                 .forEach(section => {
-                    if (!sections.value[section.title]) {
-                        sections.value[section.title] = []
+                    if (!docsStore.sections[section.title]) {
+                        docsStore.sections[section.title] = []
                     }
                 })
 
@@ -36,38 +42,19 @@ export default defineComponent({
             articles
                 .sort((a, b) => a.position - b.position)
                 .forEach(article => {
-                    if (sections.value[article.section.title]) {
-                        sections.value[article.section.title].push(article)
+                    if (docsStore.sections[article.section.title]) {
+                        docsStore.sections[article.section.title].push(article)
                     }
                 })
 
             /** pre-selected article */
-            selectArticle(sections.value[allSections[0].title][0])
-        })
-
-        const nextArticle = computed(() => {
-            if (!selectedArticle.value.section) return
-
-            const currentSection = selectedArticle.value.section.title
-            const selectedArticleIndex = sections.value[currentSection].indexOf(
-                selectedArticle.value,
-            )
-
-            if (sections.value[currentSection][selectedArticleIndex + 1]) {
-                return sections.value[currentSection][selectedArticleIndex + 1]
-            } else {
-                const allSections = Object.keys(sections.value)
-                const currentSectionIndex = allSections.indexOf(currentSection)
-
-                return (
-                    sections.value[allSections[currentSectionIndex + 1]] &&
-                    sections.value[allSections[currentSectionIndex + 1]][0]
-                )
-            }
+            selectArticle(docsStore.sections[allSections[0].title][0])
         })
 
         const selectArticle = article => {
-            selectedArticle.value = article
+            docsStore.article = article
+
+            router.push(`/docs/${article.slug.current}`)
 
             /** find content */
             const md = new Markdown()
@@ -82,27 +69,13 @@ export default defineComponent({
                 )
         }
 
-        const handleNextPage = () => {
-            selectedArticle.value = nextArticle.value
-
-            document.getElementById("app").scrollTo({
-                top: 0,
-                behavior: "smooth",
-            })
-        }
-
         return {
-            sections,
+            docsStore,
             selectArticle,
-            handleNextPage,
-            selectedArticle,
-            nextArticle,
             anchors,
             expandMenu,
         }
     },
-
-    components: { ArticleContent },
 })
 </script>
 
@@ -110,7 +83,7 @@ export default defineComponent({
     <div :class="$style.wrapper">
         <div :class="[$style.nav, expandMenu && $style.expanded]">
             <div
-                v-for="(section, index) in sections"
+                v-for="(section, index) in docsStore.sections"
                 :key="section"
                 :class="$style.section"
             >
@@ -122,7 +95,7 @@ export default defineComponent({
                     @click="selectArticle(article)"
                     :class="[
                         $style.link,
-                        selectedArticle == article && $style.active,
+                        docsStore.article == article && $style.active,
                     ]"
                 >
                     {{ article.title }}
@@ -131,23 +104,7 @@ export default defineComponent({
         </div>
 
         <div :class="$style.base">
-            <ArticleContent
-                v-if="selectedArticle.Body"
-                :source="selectedArticle.Body"
-            />
-
-            <div
-                v-if="nextArticle"
-                @click="handleNextPage"
-                :class="$style.next_btn"
-            >
-                <div :class="$style.next_txt">
-                    <span>Next Page</span>
-                    <span>{{ nextArticle.title }}</span>
-                </div>
-
-                <Icon name="arrowright" size="16" />
-            </div>
+            <router-view />
         </div>
 
         <div v-if="anchors" :class="$style.anchors">
@@ -230,50 +187,6 @@ export default defineComponent({
     max-width: 600px;
 
     margin: 50px 0 0 80px;
-}
-
-.next_btn {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    cursor: pointer;
-    max-width: 600px;
-    border-radius: 6px;
-    border: 1px solid var(--border);
-    height: 68px;
-
-    padding: 0 16px;
-    margin-bottom: 150px;
-
-    transition: border 0.2s ease;
-}
-
-.next_btn:hover {
-    border: 1px solid var(--border-highlight);
-}
-
-.next_txt {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-}
-
-.next_btn span:first-child {
-    font-size: 13px;
-    line-height: 1.1;
-    font-weight: 600;
-    color: var(--text-tertiary);
-}
-
-.next_btn span:last-child {
-    font-size: 14px;
-    font-weight: 600;
-    color: var(--text-primary);
-}
-
-.next_btn svg {
-    fill: var(--icon);
 }
 
 .anchors {
