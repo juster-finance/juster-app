5562872ba5d93dc41048a788c96e96863508e518
Sunlight <celldwellerq@gmail.com>
2021-11-23 16:31:29 +0300

    sentry init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import { ApolloClients } from "@vue/apollo-composable"
 import { createMetaManager } from "vue-meta"
 import { apollo } from "@/apollo"
 import { createPinia } from "pinia"
-import VueGtag from "vue-gtag"
 
+/** Analytics */
+import VueGtag from "vue-gtag"
 import amplitude from "amplitude-js"
 amplitude.getInstance().init("a515d3d1969a8f25c340476842b19836")
+
+/** Logging */
+import * as Sentry from "@sentry/vue"
+import { Integrations } from "@sentry/tracing"
 
 import "@/services/tools"
 import App from "./App.vue"
@@ -34,6 +39,20 @@ app.use(VueGtag, { config: { id: "G-58LD5WNLR4" } })
  * Provide
  */
 app.provide("amplitude", amplitude.getInstance())
+
+/** Logging Init */
+Sentry.init({
+    app,
+    dsn:
+        "https://a7417549147b41f2bad0065b5b5ebb3f@o189424.ingest.sentry.io/6077496",
+    integrations: [
+        new Integrations.BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            tracingOrigins: ["app.juster.fi", /^\//],
+        }),
+    ],
+    tracesSampleRate: 1.0,
+})
 
 /**
  * Global components
