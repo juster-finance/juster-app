4d5770afbd5ac9d7ba24ab8bd62ebb66889ed073
xdeq <celldwellerq@gmail.com>
2022-02-18 03:28:38 +0300

    fix ending countdown

--- src/services/utils/date.js
+++ src/services/utils/date.js
@@ -4,10 +4,13 @@ export const countdown = ({ target, now }) => {
     const distance = target - now
 
     return {
+        d: Math.floor(
+            (distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24),
+        ),
         h: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
         m: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
         s: Math.floor((distance % (1000 * 60)) / 1000),
-        d: distance,
+        distance,
     }
 }
 
