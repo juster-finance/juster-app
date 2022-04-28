41f45236074ff38c6dfbaa7dce767abce7c0f7c6
xdeq <celldwellerq@gmail.com>
2022-04-28 17:48:06 +0300

    fixes & cleaning

---
+++ src/components/local/modals/OperationModal.vue
@@ -0,0 +1,33 @@
+<script setup>
+/**
+ * UI
+ */
+import Modal from "@/components/ui/Modal"
+import Button from "@/components/ui/Button"
+
+const props = defineProps({ show: { type: Boolean } })
+</script>
+
+<template>
+    <Modal :show="show" width="500" closable @onClose="$emit('onClose')">
+        <div :class="$style.title">Operation</div>
+
+        <Button type="secondary" size="small" block
+            ><Icon name="open" size="16" /> View operation on TzKT</Button
+        >
+    </Modal>
+</template>
+
+<style module>
+.wrapper {
+}
+
+.title {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1.2;
+    color: var(--text-primary);
+
+    margin-bottom: 24px;
+}
+</style>
--- src/components/modules/profile/ProfileBase.vue
+++ src/components/modules/profile/ProfileBase.vue
@@ -88,8 +88,8 @@ export default defineComponent({
 				address: address.value,
 			})
 			events.value = positions
-				.filter((position) => position.value)
-				.map((position) => position.event)
+				.filter(position => position.value)
+				.map(position => position.event)
 		}
 
 		onMounted(() => {
@@ -152,174 +152,252 @@ export default defineComponent({
 </script>
 
 <template>
+
 	<div v-if="user && isProfileLoaded" :class="$style.wrapper">
+
 		<metainfo>
-			<template v-slot:title="{ content }"
-				>{{ content }} • Juster</template
-			>
+
+			<template v-slot:title="{ content }">
+				{{ content }} • Juster
+			</template>
+
 		</metainfo>
 
 		<h2 :class="$style.profile_title">
-			{{ isMyProfile ? "My profile" : `User account` }}
+			 {{ isMyProfile ? "My profile" : `User account` }}
 		</h2>
 
 		<div :class="$style.header">
+
 			<div :class="$style.profile">
+
 				<div :class="$style.avatar">
+
 					<Tooltip>
+
 						<img
-							:src="`https://services.tzkt.io/v1/avatars/${address}`"
+							:src="
+								`https://services.tzkt.io/v1/avatars/${address}`
+							"
 							:class="$style.image"
 							alt="avatar"
 						/>
 
-						<template v-slot:content
-							>This avatar is supported by TzKT.io</template
-						>
+						<template v-slot:content>
+							This avatar is supported by TzKT.io
+						</template>
+
 					</Tooltip>
+
 				</div>
 
 				<div @click="handleCopyAddress" :class="$style.username">
-					{{
-						`${address.slice(0, 8)}..${address.slice(
-							address.length - 3,
-							address.length,
-						)}`
-					}}
+					 {{ `${address.slice(0, 8)}..${address.slice( address.length
+					- 3, address.length, )}` }}
 					<Icon name="copy" size="14" />
+
 				</div>
+
 				<div :class="$style.status">
-					{{ isMyProfile ? accountStore.balance : balance }}
-					ꜩ
+					 {{ isMyProfile ? accountStore.balance : balance }} ꜩ
 				</div>
 
 				<div :class="$style.progress">
+
 					<div :class="$style.head">
+
 						<div :class="$style.tier">Tier 1</div>
 
 						<div :class="$style.exp">Exp 0</div>
+
 					</div>
 
 					<div :class="$style.line" />
+
 				</div>
 
 				<div :class="$style.badges">
+
 					<img
 						src="@/assets/badge.png"
 						:class="$style.badge"
 						alt="badge"
 					/>
+
 					<img
 						src="@/assets/badge.png"
 						:class="$style.badge"
 						alt="badge"
 					/>
+
 					<img
 						src="@/assets/badge.png"
 						:class="$style.badge"
 						alt="badge"
 					/>
+
 					<img
 						src="@/assets/badge.png"
 						:class="$style.badge"
 						alt="badge"
 					/>
+
 				</div>
+
 			</div>
 
 			<div :class="$style.statistics">
+
 				<h3 :class="$style.title">Statistics</h3>
 
 				<div :class="$style.block">
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Liquidity provided</div>
+
 						<div :class="$style.value">
-							{{ abbreviateNumber(user.totalLiquidityProvided) }}
+							 {{ abbreviateNumber(user.totalLiquidityProvided) }}
+
 							<span>ꜩ</span>
+
 						</div>
+
 					</div>
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Net return</div>
+
 						<div :class="$style.value">
-							{{ abbreviateNumber(user.totalProviderReward) }}
+							 {{ abbreviateNumber(user.totalProviderReward) }}
 							<span>ꜩ</span>
+
 						</div>
+
 					</div>
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Fees collected</div>
+
 						<div :class="$style.value">
-							{{ user.totalFeesCollected.toFixed(0) }}
+							 {{ user.totalFeesCollected.toFixed(0) }}
 							<span>ꜩ</span>
+
 						</div>
+
 					</div>
+
 				</div>
 
 				<div :class="$style.divider" />
 
 				<div :class="$style.block">
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Bets value</div>
+
 						<div :class="$style.value">
-							{{ user.totalBetsAmount }}
+							 {{ user.totalBetsAmount }}
 							<span>ꜩ</span>
+
 						</div>
+
 					</div>
+
 					<div v-if="user.totalWithdrawn" :class="$style.stat">
+
 						<div :class="$style.key">Withdrawn</div>
+
 						<div :class="$style.value">
-							{{ abbreviateNumber(user.totalWithdrawn) }}
+							 {{ abbreviateNumber(user.totalWithdrawn) }}
 							<span>ꜩ</span>
+
 						</div>
+
 					</div>
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Bets</div>
+
 						<div :class="$style.value">
-							{{ user.totalBetsCount }}
+							 {{ user.totalBetsCount }}
 						</div>
+
 					</div>
+
 					<div :class="$style.stat">
+
 						<div :class="$style.key">Favorite Market</div>
+
 						<div :class="$style.value">TBD</div>
+
 					</div>
+
 				</div>
 
 				<div :class="$style.divider" />
 
 				<div :class="$style.additional">
+
 					<div :class="$style.left">
+
 						<a
-							:href="`https://${
-								currentNetwork == 'mainnet' ? '' : 'ithacanet.'
-							}tzkt.io/${address}`"
+							:href="
+								`https://${
+									currentNetwork == 'mainnet'
+										? ''
+										: 'ithacanet.'
+								}tzkt.io/${address}`
+							"
 							target="_blank"
 						>
+
 							<Button type="secondary" size="small">
-								<Icon name="open" size="14" />View on TzKT
+
+								<Icon name="open" size="14" />
+								View on TzKT
 							</Button>
+
 						</a>
+
 					</div>
 
 					<div :class="$style.right"></div>
+
 				</div>
+
 			</div>
+
 		</div>
 
 		<div v-if="isMyProfile && events.length" :class="$style.submissions">
+
 			<div :class="$style.top">
+
 				<div>
+
 					<h2>My submissions</h2>
+
 					<div :class="$style.description">
-						List of all current and archived events
+						 List of all current and archived events
 					</div>
+
 				</div>
+
 			</div>
 
 			<div :class="$style.items">
+
 				<EventCard
 					v-for="event in paginatedEvents"
 					:key="event.id"
 					:event="event"
 				/>
+
 			</div>
 
 			<Pagination
@@ -329,10 +407,13 @@ export default defineComponent({
 				:limit="6"
 				:class="$style.pagination"
 			/>
+
 		</div>
+
 	</div>
 
 	<div v-if="!user && isProfileLoaded" :class="$style.empty_profile">
+
 		<img
 			:src="`https://services.tzkt.io/v1/avatars/${accountStore.pkh}`"
 			:class="$style.error_avatar"
@@ -340,25 +421,34 @@ export default defineComponent({
 		/>
 
 		<div :class="$style.error_title">Your profile is not ready yet</div>
+
 		<div :class="$style.error_description">
-			Once you participate in any event, your profile will become
-			available!
+			 Once you participate in any event, your profile will become available!
+
 		</div>
 
 		<div :class="$style.error_buttons">
+
 			<router-link to="/">
+
 				<Button type="secondary" size="small">
-					<Icon name="spark" size="14" />Explore Juster
+
+					<Icon name="spark" size="14" />
+					Explore Juster
 				</Button>
+
 			</router-link>
 
 			<div :class="$style.error_description">or</div>
 
-			<Button @click="handleBack" type="secondary" size="small"
-				>Go back</Button
-			>
+			<Button @click="handleBack" type="secondary" size="small">
+				Go back
+			</Button>
+
 		</div>
+
 	</div>
+
 </template>
 
 <style module>
@@ -642,3 +732,4 @@ export default defineComponent({
 	margin-top: 24px;
 }
 </style>
+
