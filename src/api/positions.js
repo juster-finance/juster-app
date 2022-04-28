e64493ebb7262e1f3e6bb3b153fc898a9e205b83
xdeq <celldwellerq@gmail.com>
2022-04-28 18:07:27 +0300

    new eslint config

--- src/api/positions.js
+++ src/api/positions.js
@@ -9,38 +9,38 @@ import { juster } from "@/services/sdk"
  * GQL: Queries
  */
 import {
-    getAllUserPositions,
-    getUserPositionsForWithdrawal,
+	getAllUserPositions,
+	getUserPositionsForWithdrawal,
 } from "@/graphql/queries/positions"
 
 export const fetchAllUserPositions = async ({ address }) => {
-    try {
-        const { data } = await juster.apollo.query({
-            query: getAllUserPositions,
-            variables: { address },
-        })
+	try {
+		const { data } = await juster.apollo.query({
+			query: getAllUserPositions,
+			variables: { address },
+		})
 
-        return data.position
-    } catch (error) {
-        console.error(
-            `Error during fetching all user positions \n\n ${error.name}: ${error.message}`,
-        )
-        return []
-    }
+		return data.position
+	} catch (error) {
+		console.error(
+			`Error during fetching all user positions \n\n ${error.name}: ${error.message}`,
+		)
+		return []
+	}
 }
 
 export const fetchUserPositionsForWithdraw = async ({ address }) => {
-    try {
-        const { data } = await juster.apollo.query({
-            query: getUserPositionsForWithdrawal,
-            variables: { address },
-        })
+	try {
+		const { data } = await juster.apollo.query({
+			query: getUserPositionsForWithdrawal,
+			variables: { address },
+		})
 
-        return cloneDeep(data.position)
-    } catch (error) {
-        console.error(
-            `Error during fetching user positions \n\n ${error.name}: ${error.message}`,
-        )
-        return []
-    }
+		return cloneDeep(data.position)
+	} catch (error) {
+		console.error(
+			`Error during fetching user positions \n\n ${error.name}: ${error.message}`,
+		)
+		return []
+	}
 }
