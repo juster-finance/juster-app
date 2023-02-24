bb92fb0d67884bc7744ab896bd9fa856b112481d
xdeq <celldwellerq@gmail.com>
2023-02-25 01:21:22 +0300

    blog & releases: user-select text

--- src/views/Other/BlogPage.vue
+++ src/views/Other/BlogPage.vue
@@ -11,7 +11,13 @@ import BlogBase from "@modules/blog/BlogBase.vue"
 </script>
 
 <template>
-	<Page width="1080">
+	<Page width="1080" :class="$style.wrapper">
 		<BlogBase />
 	</Page>
 </template>
+
+<style module>
+.wrapper {
+	user-select: text;
+}
+</style>
